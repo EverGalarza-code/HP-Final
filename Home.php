d42e482042e1d94e66340f42332eb0f65b79ba37
Ever-galarza <Evergalarza30@gmail.com>
2024-11-07 00:40:32 -0600

    t

--- Home.php
+++ Home.php
@@ -4,7 +4,7 @@
     <title>Holopets | Home</title>
     <link rel="icon" type="image/x-icon" href="images/Logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="styless.css">
+    <link rel="stylesheet" type="text/css" href="stylesssss.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
