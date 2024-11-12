3273f9d14b55322a6f392a1b6e2fb056320e048d
Ever-galarza <Evergalarza30@gmail.com>
2024-11-11 20:58:50 -0600

    g

--- Home.php
+++ Home.php
@@ -8,7 +8,7 @@ session_start();
     <title>Holopets | Home</title>
     <link rel="icon" type="image/x-icon" href="images/Logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="stylesss.css">
+    <link rel="stylesheet" type="text/css" href="styless.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -39,7 +39,6 @@ session_start();
             <input type="email" name="mailuid" placeholder="your email" class="box">
             <input type="password" name="pwd" placeholder="your password" class="box">
 
-            <p>Forget Your Password <a href="#"> Click Here</a></p>
             <p>Dont Have An Account <a href="index.php" class="login-link"> Create Now</a></p>
 
             <input type="submit" name="login-submit" value="Login Now" class="btn">
